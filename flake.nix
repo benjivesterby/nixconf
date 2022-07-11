a31111b461bf96e03aef8b3d1cce59e0c10f2df0
Benji Vesterby <benji@devnw.com>
2022-07-11 06:27:19 -0400

    Initial system commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Primary Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+  let
+    user = "benji";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+    	inherit system;
+      config.allowUnfree = true;
+    };
+
+    lib = nixpkgs.lib;
+  in {
+
+      nixosConfigurations = {
+        Gopher = lib.nixosSystem {
+          inherit system;
+          modules = [ 
+	    ./configuration.nix
+            home-manager.nixosModules.home-manager {
+	      home-manager.useGlobalPkgs = true;
+	      home-manager.useUserPackages = true;
+	      home-manager.users.${user} = {
+	        imports = [ ./home.nix ];
+	      };
+	    }
+	  ];
+        };
+      };
+  };
+}
